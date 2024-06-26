d488db60421dfad7f0fd9873d255673378e697ab
Mohamed Mosa <mohamed24halim@gmail.com>
2024-06-26 05:26:33 +0300

    End

--- Age-Calculator/main.js
+++ Age-Calculator/main.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
         let month = document.querySelector('.month_').value;
         let year = document.querySelector('.year_').value;
 
+
+        let err_day = document.querySelector('.error-day');
+        let err_month = document.querySelector('.error-month');
+        let err_year = document.querySelector('.error-year');
+
+        let err_display1 = document.getElementById('error-year');
+        let err_display2 = document.getElementById('error-month');
+        let err_display3 = document.getElementById('error-day');
         var now = new Date();
 
         // Check numbers
@@ -21,34 +29,36 @@ document.addEventListener("DOMContentLoaded", () => {
             {
                 if (day > 31)
                 {
-                    let err_day = document.querySelector('.error-day');
                     err_day.classList.add('active');
 
-                    err_display = document.getElementById('error-day');
-                    err_display.style.display = 'block';
+                    err_display3.style.display = 'block';
 
                 }
                 if (month > 12)
                 {
-                    let err_month = document.querySelector('.error-month');
                     err_month.classList.add('active');
 
-                    err_display = document.getElementById('error-month');
-                    err_display.style.display = 'block';
+                    err_display2.style.display = 'block';
 
                 }
                 if (year > now.getFullYear())
                 {
-                    let err_year = document.querySelector('.error-year');
                     err_year.classList.add('active');
 
-                    err_display = document.getElementById('error-year');
-                    err_display.style.display = 'block';
+                    err_display1.style.display = 'block';
 
                 }
             }
             else
             {
+                // error state
+                err_day.classList.remove('active');
+                err_month.classList.remove('active');
+                err_year.classList.remove('active');
+                err_display1.style.display = 'none';
+                err_display2.style.display = 'none';
+                err_display3.style.display = 'none';
+
                 let new_year = now.getFullYear() - year;
                 let new_month = now.getMonth() - month;
                 let new_day = now.getDate() - day;
@@ -75,6 +85,36 @@ document.addEventListener("DOMContentLoaded", () => {
                 txt_day.textContent = Math.abs(new_day);
                 
             }
+        }
+        else
+        {
+            if (day > 31 || day == '')
+                {
+                    let err_day = document.querySelector('.error-day');
+                    err_day.classList.add('active');
+
+                    err_display = document.getElementById('error-day');
+                    err_display.style.display = 'block';
+
+                }
+                if (month > 12 || month == '')
+                {
+                    let err_month = document.querySelector('.error-month');
+                    err_month.classList.add('active');
+
+                    err_display = document.getElementById('error-month');
+                    err_display.style.display = 'block';
+
+                }
+                if (year > now.getFullYear() || year == '')
+                {
+                    let err_year = document.querySelector('.error-year');
+                    err_year.classList.add('active');
+
+                    err_display = document.getElementById('error-year');
+                    err_display.style.display = 'block';
+
+                }
         }
 
     });
