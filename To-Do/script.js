00c7bee1ea51b2f4350592c81b58a59a8082b585
Mohamed Mosa <mohamed24halim@gmail.com>
2024-06-29 19:44:50 +0300

    end

--- To-Do/script.js
+++ To-Do/script.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     let sun = document.querySelector('.sun');
-    console.log(sun);
     // light theme
     sun.onclick = () => {
         const light_theme = document.querySelectorAll('.theme');
         light_theme.forEach((ele) => {
             ele.classList.toggle('light');
+            
         });
         saveData();
     };
 
+    // var allElements = document.querySelectorAll('*');
+    //         // Apply a class to each element
+    //         allElements.forEach(function(element) {
+    //             element.classList.add('highlight');
+    //         });
 
     // list
     let ul = document.querySelector('ul');
@@ -18,9 +23,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // ADD NEW TASK
     let input_btn = document.querySelector('.span_input');
-    const input = document.querySelector('.taskes'); 
+    const input = document.querySelector('.taskes');
+
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter')
+        {
+            add_task();   
+        }
+    });
     input_btn.onclick = () => {
-        
+        add_task() 
+    }
+    function add_task()
+    {
         if (input.value === '')
         {
             alert('You must write somthing');
@@ -29,14 +44,15 @@ document.addEventListener("DOMContentLoaded", () => {
         {
             let new_li = document.createElement('li');
             new_li.classList.add('li');
+            new_li.classList.add('theme');
 
             let new_span = document.createElement('span');
             new_span.classList.add('span_li')
             new_span.classList.add('li')
+            new_span.classList.add('theme');
 
             new_li.appendChild(new_span);
             new_li.innerHTML += input.value;
-
 
             ul.appendChild(new_li);
         }
@@ -52,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
             saveData();
         }
         else if (e.target.tagName == 'SPAN'){
-            e.target.parentElement.classList.toggle('txt_complete');
+            e.target.parentElement.classList.toggle('txt_complete'); 
             saveData();
         }
     });
@@ -61,16 +77,20 @@ document.addEventListener("DOMContentLoaded", () => {
     let clear = document.querySelector('.right');
     clear.addEventListener('click', () => {
         let li = document.querySelectorAll('li');
-
+        let p_ = document.querySelector('p');
         li.forEach((ele) => {
             if (ele.classList.contains('txt_complete')) {
-                ele.style.display = 'none';
+                ele.remove();
             }
         });
         saveData();
     });
     //  COUNTER
-    
+    const counter = document.querySelector('.numberoftaskes');
+    setInterval(() => {
+        let li = document.querySelectorAll('li');
+        counter.textContent = li.length;
+    }, 100)
 
     // SAVING DATA
 
